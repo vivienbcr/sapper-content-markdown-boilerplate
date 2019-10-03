d21b5006b428e5266af9707770fd71ea267519be
Vivien <Vivien.boucher@outlook.com>
2019-10-03 18:47:27 +0200

    init

---
+++ src/routes/posts/index.json.js
@@ -0,0 +1,31 @@
+import fm from "front-matter";
+import glob from "glob";
+import { fs } from "mz";
+import path from "path";
+
+export async function get(req, res) {
+	// List the Markdown files and return their filenames
+	const posts = await new Promise((resolve, reject) =>
+		glob("static/_posts/*.md", (err, files) => {
+			if (err) return reject(err);
+			return resolve(files);
+		})
+	);
+
+	// Parse out the metadata from the files
+	const postsFrontMatter = await Promise.all(
+		posts.map(async post => {
+			const content = (await fs.readFile(post)).toString();
+			return { ...fm(content).attributes, slug: path.parse(post).name };
+		})
+	);
+
+	// Sort by reverse datea, because ait's a blog
+	postsFrontMatter.sort((a, b) => (a.date < b.date ? 1 : -1));
+
+	res.writeHead(200, {
+		"Content-Type": "application/json"
+	});
+
+	res.end(JSON.stringify(postsFrontMatter));
+}
---
+++ static/_posts/hello.md
@@ -0,0 +1,10 @@
+---
+layout: blog
+title: Hello markdown
+date: 2019-09-29T18:46:36.516Z
+---
+
+<h1>hello</h1>
+
+This is a test post!
+
